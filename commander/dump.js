4a9d8ada98a9a9451a50de409238c81f3f89b9b7
Christian Bewernitz <coder@karfau.de>
2020-01-01 07:38:23 +0100

    impl dump for commander

--- commander/dump.js
+++ commander/dump.js
@@ -1,6 +1,14 @@
 const commander = require('commander');
 //TODO: add the simplest code to support entries in ../dump.json
-const parsed = process.argv;
+const c = commander
+  .option('--flag')
+  .option('--count', 'counts', (_, prev = 0) => prev + 1)
+  .option('--str <val>')
+  .option('--arr <val>', 'collects', (cur, prev = []) => {
+    prev.push(cur);
+    return prev;
+  })
+  .arguments('[first] [second]')
+  .parse(process.argv)
+  const parsed = {opts: c.opts(), args: c.args};
 console.log(JSON.stringify(parsed))
-//TODO: remove this line and the next one to mark as ready
-process.exit(255)
