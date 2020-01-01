fb8f511de58df13f67a85e236d1c73b5c0d50f2c
Christian Bewernitz <coder@karfau.de>
2020-01-01 21:56:45 +0100

    impl dump for dashdash

--- dashdash/dump.js
+++ dashdash/dump.js
@@ -1,6 +1,24 @@
 const dashdash = require('dashdash');
-//TODO: add the simplest code to support entries in ../dump.json
-const parsed = process.argv;
+
+const count = {};
+dashdash.addOptionType({
+  name: 'count',
+  takesArg: false,
+  parseArg: function (_, optstr) {
+    let current = count[optstr] || 0;
+    current++
+    count[optstr] = current;
+    return current;
+  }
+});
+const options = [
+  {name: 'flag', type: 'bool'},
+  {name: 'count', type: 'count'},
+  {name: 'str', type: 'string'},
+  {name: 'arr', type: 'arrayOfString'},
+  {name: 'help', type: 'bool'}
+];
+const parsed = dashdash.parse({options});
 console.log(JSON.stringify(parsed))
 //TODO: remove this line and the next one to mark as ready
 process.exit(255)
