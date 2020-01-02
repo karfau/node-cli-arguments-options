1b09264540c16faa821221f0a79c5220f9497ba9
Christian Bewernitz <coder@karfau.de>
2020-01-02 22:35:36 +0100

    completed dump.js impl for args

--- args/dump.js
+++ args/dump.js
@@ -1,11 +1,18 @@
 const args = require('args');
 args.option('flag')
-args.option('count', 'counts', []) // FIXME: can we tune it to count?
+args.option('count', 'counts')
 args.option('str')
-args.option('arr', 'collect', []) // FIXME: string vs array value?
-// TODO: 'first'/'second' are not present in output
+args.option('arr', 'collect', [])
+const help = false;
+!help && args.option('help') // needs to be added manually when dis
 
-const parsed = args.parse(process.argv);
-console.log(JSON.stringify(parsed))
-//TODO: remove this line and the next one to mark as ready
-process.exit(255)
+const parsed = args.parse(
+  process.argv, {exit: {help}, help}
+);
+// count requires custom code after parsing:
+if (parsed.count) {
+  parsed.count = parsed.count === true ? 1 : parsed.count.length
+}
+// sync automatic alias
+parsed.c = parsed.count
+console.log(JSON.stringify({parsed, sub: args.sub}))
