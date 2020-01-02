f5fb648d0ad1bd65dff15eb2c288e84724136380
Christian Bewernitz <coder@karfau.de>
2020-01-02 22:42:57 +0100

    add.js: automate execution of `about.js`, `dump.js` and `git add`

--- add.js
+++ add.js
@@ -4,18 +4,21 @@ const {existsSync, mkdirpSync, writeFileSync} = require('fs-extra');
 
 function run (pkg) {
   const dir = `${pkg}`
+  const execOpts = {stdio: 'inherit', cwd: dir}
   if (!existsSync(dir)) mkdirpSync(dir);
-  process.chdir(dir)
-  execSync('npm init --scope=cliao -y', {stdio: 'inherit'})
-  execSync(`npm i -D ${pkg}`, {stdio: 'inherit'})
+  execSync('npm init --scope=cliao -y', execOpts)
+  execSync(`npm i -D ${pkg}`, execOpts)
   const dumpFile = 'dump.js'
-  /*if (!existsSync(dumpFile)) */ writeFileSync(dumpFile, `const ${camelcase(pkg)} = require('${pkg}');
+  if (!existsSync(dumpFile))  writeFileSync(dumpFile, `const ${camelcase(pkg)} = require('${pkg}');
 //TODO: add the simplest code to support entries in ../dump.json
 const parsed = process.argv;
 console.log(JSON.stringify(parsed))
 //TODO: remove this line and the next one to mark as ready
 process.exit(255)
 `);
+  execSync(`runex about ${pkg}`)
+  execSync(`runex dump ${pkg}`)
+  execSync(`git add ${pkg}`)
 }
 
 module.exports = {
