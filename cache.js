92d8415124ec5f5622ea1a4f417f1fb0f6de049a
Christian Bewernitz <coder@karfau.de>
2020-01-03 10:09:25 +0100

    improved cache.js
    - is now able to create cache for anonymous functions correctly (long file names!)
    - broken cache file no longer makes script file
    - doc comments
    - sort imports

--- cache.js
+++ cache.js
@@ -1,28 +1,30 @@
 const { mkdir, pathExists, readJson, writeJson } = require('fs-extra')
 const path = require('path')
 
-
 const CACHE_DIR = path.join(__dirname, '.cache')
 
 /**
+ * Stores the values returned by `resolve` inside `./.cache` as JSON files.
  *
- * @param mainKey
- * @param {Function} resolve
- * @param keys
+ * @param {string} mainKey the first part of the cache file name
+ * @param {Function} resolve The method to invoke (with `keys`) to get the data.
+ *        Will not be invoked if the data is in the cache.
+ *        It's name (or the code for anonymous functions) is also part of the cache file name.
+ * @param {any} keys The keys are joined to form the key inside the cache file. They are also passed to `resolve`
  * @returns {Promise<ReturnType<resolve>>}
  */
 async function cached(mainKey, resolve, ...keys) {
   if (!await pathExists(CACHE_DIR)) {
     await mkdir(CACHE_DIR)
   }
-  if (!resolve.name) console.error(
-    'incomplete cache key for anonymous function', mainKey, resolve.toString()
-  )
-  const hash = [mainKey, resolve.name].join('.').replace(/\//g, '--') + '.json'
+  const hash = [mainKey, resolve.name || resolve].join('.').replace(/\/|\s+/g, '--') + '.json'
   const file = path.join(CACHE_DIR, hash)
-  const cache = await pathExists(file) ? await readJson(file) : {}
-  const cacheKey = keys.join(':')
+  let cache = {};
+  try {
+    cache = await readJson(file)
+  } catch {}
 
+  const cacheKey = keys.join(':')
   if (cacheKey in cache) {
     return cache[cacheKey]
   }
