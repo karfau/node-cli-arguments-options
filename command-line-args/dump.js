aa0a3d4ecd20f5fa0ad7128e0f799c418bcc24b4
Christian Bewernitz <coder@karfau.de>
2020-01-13 21:47:14 +0100

    test(features/command-line-args): all features specified, most verified
    `{"features":24,"specified":7,"verified":17,"checks":35}`
    - some additions in arg/dump.js as suggested in #6

--- command-line-args/dump.js
+++ command-line-args/dump.js
@@ -4,12 +4,13 @@ const {arguments = [], ...options} = commandLineArgs([
   {name: 'flag', type: Boolean},
   // https://github.com/75lb/command-line-args/issues/97
   {name: 'arguments', type: String, multiple: true, defaultOption: true},
-  {name: 'count', type: Boolean, multiple: true},
+  {name: 'count', alias: 'c', type: Boolean, multiple: true},
   {name: 'num', type: parseFloat},
   {name: 'int', type: parseInt},
   {name: 'date', type: Date.parse},
   {name: 'str', type: String},
-  {name: 'arr', type: String, multiple: true, lazyMultiple: true},
+  {name: 'arr', type: String, lazyMultiple: true},
+  {name: 'mult', type: String, multiple: true},
   {name: 'help', type: Boolean},
 ])
 // count requires custom code after parsing:
