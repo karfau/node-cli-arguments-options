3e2dcc8fe47dc1d06b05c04ed4ddf240e6e1f807
Christian Bewernitz <coder@karfau.de>
2020-02-01 07:37:34 +0100

    test(features/commander): Negative numbers don't need `=`
    Added "custom option handling" for integer and number.
    
    https://github.com/zeit/arg/issues/52

--- commander/dump.js
+++ commander/dump.js
@@ -3,6 +3,8 @@ const commander = require('commander');
 const c = commander
   .option('--flag')
   .option('--count', 'counts', (_, prev = 0) => prev + 1)
+  .option('--int <integer>', 'integer', (s) => parseInt(s, 10))
+  .option('--num <number>', 'number', parseFloat)
   .option('--str <val>')
   .option('--arr <val>', 'collects', (cur, prev = []) => {
     prev.push(cur);
